3ff784e94348307052985f697ccbd277b026453a
lhx-01 <2182794453@qq.com>
2020-10-08 12:57:26 +0800

    记账本管理系统

---
+++ BookKeeping/src/main/java/com/kgc/service/impl/billsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.kgc.service.impl;
+
+import com.kgc.mapper.BillsMapper;
+import com.kgc.pojo.Bills;
+import com.kgc.pojo.BillsExample;
+import com.kgc.service.billsService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("billsService")
+public class billsServiceImpl implements billsService {
+    @Resource
+    BillsMapper billsMapper;
+
+    @Override
+    public List<Bills> selectByExample2(BillsExample example) {
+        return billsMapper.selectByExample2(example);
+    }
+
+    @Override
+    public int insert(Bills record) {
+        return billsMapper.insert(record);
+    }
+}
